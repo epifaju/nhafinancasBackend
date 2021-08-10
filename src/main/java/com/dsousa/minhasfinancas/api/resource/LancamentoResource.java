4332369565ac8ec6d8d566f61ae2bef56608a823
Cassama-Goncalves Epifanio <epifanio.cassama-goncalves@inetum.com>
2021-08-10 16:49:40 +0200

    suite nha financas

--- src/main/java/com/dsousa/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/dsousa/minhasfinancas/api/resource/LancamentoResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dsousa.minhasfinancas.api.dto.AtualizaStatusDTO;
 import com.dsousa.minhasfinancas.api.dto.LancamentoDTO;
 import com.dsousa.minhasfinancas.exception.RegraNegocioException;
 import com.dsousa.minhasfinancas.model.entity.Lancamento;
@@ -81,6 +82,24 @@ public class LancamentoResource {
 				return ResponseEntity.ok(lancamento);
 			} catch (RegraNegocioException e) {
 				
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+		}).orElseGet(() -> new ResponseEntity("Lancamento nao encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
+		
+	}
+	
+	@PutMapping("{id}/atualiza-status")
+	public ResponseEntity atualizarStatus (@PathVariable("id") Long id, @RequestBody AtualizaStatusDTO dto) {
+		return service.obterPorId(id).map(entity -> {
+			StatusLancamento statusSelecionado =  StatusLancamento.valueOf(dto.getStatus());
+			if(statusSelecionado == null) {
+				return ResponseEntity.badRequest().body("Nao foi possivel atualizar o status do lançamento, envie um status valido.");
+			}
+			try {
+				entity.setStatus(statusSelecionado);
+				service.atualizar(entity);
+				return ResponseEntity.ok(entity);
+			} catch (RegraNegocioException e) {
 				return ResponseEntity.badRequest().body(e.getMessage());
 			}
 		}).orElseGet(() -> new ResponseEntity("Lancamento nao encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
